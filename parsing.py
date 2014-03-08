ab4cc4c470c64f6e238e04746a9ed49af7e76a16
ChosunOne <koreanhalo@gmail.com>
2014-03-08 15:57:22 -0600

    Modified parsing.py Josiah

--- parsing.py
+++ parsing.py
@@ -12,31 +12,31 @@ cuisines =[]
 ingredients_descriptor = []
 
 #define ingredient categories
-ingredient_category.append('0100')		#	Dairy and Egg Products
-ingredient_category.append('0200')		#	Spices and Herbs
-ingredient_category.append('0300')		#	Baby Foods
-ingredient_category.append('0400')		#	Fats and Oils
-ingredient_category.append('0500')		#	Poultry Products
-ingredient_category.append('0600')		#	Soups, Sauces, and Gravies
-ingredient_category.append('0700')		#	Sausages and Luncheon Meats
-ingredient_category.append('0800')		#	Breakfast Cereals
-ingredient_category.append('0900')		#	Fruits and Fruit Juices
-ingredient_category.append('1000')		#	Pork Products
-ingredient_category.append('1100')		#	Vegetables and Vegetable Products
-ingredient_category.append('1200')		#	Nut and Seed Products
-ingredient_category.append('1300')		#	Beef Products
-ingredient_category.append('1400')		#	Beverages
-ingredient_category.append('1500')		#	Finfish and Shellfish Products
-ingredient_category.append('1600')		#	Legumes and Legume Products
-ingredient_category.append('1700')		#	Lamb, Veal, and Game Products
-ingredient_category.append('1800')		#	Baked Products
-ingredient_category.append('1900')		#	Sweets
-ingredient_category.append('2000')		#	Cereal Grains and Pasta
-ingredient_category.append('2100')		#	Fast Foods
-ingredient_category.append('2200')		#	Meals, Entrees, and Side Dishes
-ingredient_category.append('2500')		#	Snacks
-ingredient_category.append('3500')		#	American Indian/Alaska Native Foods
-ingredient_category.append('3600')		#	Restaurant Foods
+#~0100~^~Dairy and Egg Products~
+#~0200~^~Spices and Herbs~
+#~0300~^~Baby Foods~
+#~0400~^~Fats and Oils~
+#~0500~^~Poultry Products~
+#~0600~^~Soups, Sauces, and Gravies~
+#~0700~^~Sausages and Luncheon Meats~
+#~0800~^~Breakfast Cereals~
+#~0900~^~Fruits and Fruit Juices~
+#~1000~^~Pork Products~
+#~1100~^~Vegetables and Vegetable Products~
+#~1200~^~Nut and Seed Products~
+#~1300~^~Beef Products~
+#~1400~^~Beverages~
+#~1500~^~Finfish and Shellfish Products~
+#~1600~^~Legumes and Legume Products~
+#~1700~^~Lamb, Veal, and Game Products~
+#~1800~^~Baked Products~
+#~1900~^~Sweets~
+#~2000~^~Cereal Grains and Pasta~
+#~2100~^~Fast Foods~
+#~2200~^~Meals, Entrees, and Side Dishes~
+#~2500~^~Snacks~
+#~3500~^~American Indian/Alaska Native Foods~
+#~3600~^~Restaurant Foods~
 
 def buildRecipeObject(recipeInfo):#recipeInfo is a dictionary
 	recipe = Recipe(name=recipeInfo['title'],author= recipeInfo['author'], cooktime=recipeInfo['time'], servings=recipeInfo['servings'], rating=recipeInfo['rating'])
@@ -57,5 +57,109 @@ def main(recipeURL):
 	recipe = buildRecipeObject(recipeInfo)
 	print recipe.unicode
 
+def tokenizeLine(string):
+	
+	line = string.split('^')
+	numArgs = len(line)
+	out = []
+
+	for item in line:
+
+		if item == '':
+
+			out.append(item)
+
+		else:
+
+			if item[0] != '~':
+
+				out.append(item)
+
+			else:
+
+				text = item.split('~')
+				empty = False
+				appended = False
+
+				for word in text:
+
+					if len(text) == 3 and text[1] == word and word != '':
+
+						out.append(word)
+						appended = True
+
+					if empty and word == '' and not appended:
+
+						out.append(word)
+						empty = False
+						appended = True
+
+					else:
+
+						if word == '':
+
+							empty = True
+
+	return out
+
+def readIngredientFromLine(line):
+
+	#fields = 0
+
+	#if objectType == 'FOOD':
+		
+	#	isFood = True
+	#	fields = 14
+
+	#elif objectType == 'NUTIRENT':
+
+	#	isNutrient = True
+	#	fields = 6
+
+	#else:
+
+	#	print('objectType not recognized.')
+	#	return None
+
+	tokens = tokenizeLine(line)
+
+	
+
+	output = ingredient()
+
+	#	output.id = tokens[0]
+	output.category = tokens[1]
+	output.descriptor = tokens[2]
+	#	output.shortDesc = tokens[3]
+	output.name = tokens[4]
+	#	output.manufacName = tokens[5]
+	#	output.survey = tokens[6]
+	#	output.refDesc = tokens[7]
+	#	output.refuse = tokens[8]
+	#	output.sciName = tokens[9]
+	#	output.nFactor = tokens[10]
+	#	output.proFactor = tokens[11]
+	#	output.fatFactor = tokens[12]
+	#	output.carbFactor = tokens[13]
+
+	return output
+
+def readIngredientsFromFile(fileName):
+
+	ingredientList = []
+
+	with open(fileName, 'r', encoding="utf8") as f:
+
+		while(True):
+
+			line = f.readline()
+
+			if not line:
+
+				break
+
+			ingredientList.append(readIngredientFromLine(line))
+
+	return ingredientList
 
 main(sys.argv[1])
