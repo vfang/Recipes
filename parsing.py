7535729461edf1761b90ba75fc17e1c68e45d8b2
ChosunOne <koreanhalo@gmail.com>
2014-03-10 17:48:08 -0500

    Changed from import to import
    Josiah
    Now call the file name like this:
    filename.function(args)

--- parsing.py
+++ parsing.py
@@ -1,6 +1,6 @@
-from scraper import retrieveRecipe
-from objects import *
-from lists import *
+import scraper
+import objects
+import lists
 import pprint
 import sys
 import re
@@ -61,28 +61,53 @@ tools =["Apple corer", "Basting",
 #~3600~^~Restaurant Foods~
 
 def buildRecipeObject(recipeInfo):#recipeInfo is a dictionary
-	recipe = Recipe(name=recipeInfo['title'],author= recipeInfo['author'], cooktime=recipeInfo['time'], servings=recipeInfo['servings'], rating=recipeInfo['rating'])
+	recipe = objects.Recipe(name=recipeInfo['title'],
+		author= recipeInfo['author'], 
+		cooktime=recipeInfo['time'], 
+		servings=recipeInfo['servings'], 
+		rating=recipeInfo['rating'])
+
 	directions = parseDirections(recipeInfo['directions'])
-	ingredients = parseIngredients(recipeInfo['ingredients'])
+	recipe.ingredients = parseIngredients(recipeInfo['ingredients'])
 	recipe.directions = recipeInfo['directions']
-	recipe.tools= directions.tools
-	recipe.methods = directions.methods
-	recipe.ingredients= ingredients
+	recipe.tools= directions[tools]
+	recipe.methods = directions[methods]
+	#recipe.ingredients= ingredients
 	return recipe
 
-def parseIngredients(ingredients):#return ingredient object for each ingredient and find a descriptor and preparation
-	for ing in ingredients:
-		search = {}
-		items = ing.split()
+def parseIngredient(ingr):#Maps a string to the corresponding ingredient in the ingredient database
+	ing = objects.ingredient()
+	items = ingr.split()
+	matches = []
+	matchScore = 0.0
 
-		for DBing in lists.ingredientDB:
-			match = True
+	for DBing in lists.ingredientDB:
+		match = True
 
-			for word in items:
-				if word not in DBing.descriptor:
-					match = False
+		for word in items:
+			if word not in DBing.descriptor:
+				match = False
+				break
+			else:
+				des = DBing.descriptor.split(',')
+				for d in des:
+					if word in d:
+						matchScore += 1.0 / (des.index(d) + .001)
+
 		if match:
-			search[ing] = lists.ingNameDB[ing]
+			ing.name = DBing.name
+			ing.descriptor = DBing.descriptor
+			break
+
+	return ing
+
+def parseIngredients(ingredients):
+	ings = []
+	for ing in ingredients:
+		ing.append(parseIngredient(ing))
+
+	return ings
+
 
 def parseDirections(directions):#return a dictionary with directions, tools, and methods
 	pass
@@ -163,7 +188,7 @@ def readIngredientFromLine(line):
 
 	
 
-	output = ingredient()
+	output = objects.ingredient()
 
 	#	output.id = tokens[0]
 	output.category = tokens[1]
@@ -186,7 +211,7 @@ def readIngredientsFromFile(fileName):
 
 	ingredientList = []
 
-	with open(fileName, 'r', encoding="utf8") as f:
+	with open(fileName, 'r') as f:
 
 		while(True):
 
@@ -200,4 +225,4 @@ def readIngredientsFromFile(fileName):
 
 	return ingredientList
 
-main(sys.argv[1])
+#main(sys.argv[1])
