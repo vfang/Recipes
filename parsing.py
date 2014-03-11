04172f816e81c09c41bcf0b88a56673c82d6b136
Salil Gupta <salil.gupta323@gmail.com>
2014-03-11 18:34:26 -0500

    Removes redundant tools from the tools list. Don't try and run parsing we still need to meet and synthesize all the work together into one cohesive parsing script.

--- parsing.py
+++ parsing.py
@@ -36,9 +36,8 @@ def buildRecipeObject(recipeInfo):#recipeInfo is a dictionary
 		cooktime=recipeInfo['time'], 
 		servings=recipeInfo['servings'], 
 		rating=recipeInfo['rating'])
-
-	directions = parseDirections(recipeInfo['directions'])
 	recipe.ingredients = parseIngredients(recipeInfo['ingredients'])
+	directions = parseDirections(recipeInfo['directions'])	
 	recipe.directions = recipeInfo['directions']
 	recipe.tools= directions['tools']
 	recipe.methods = directions['methods']
@@ -146,31 +145,47 @@ def parseIngredients(ingredients):
 	return ings
 
 def parseDirections(directions):#return a dictionary with directions, tools, and methods
-	ingredientsList = ['brown sugar','barbecue sauce']
 	exclude = set(string.punctuation)
+	ingredientsList =['barbecue sauce']
+	skip = False
 	words = []
 	parsed = {"tools":[],"methods":[]}
-	ignoreWords = ['a','the','in','at','and','or','on','to']
+	#ignoreWords = ['a','the','in','at','and','or','on','to']
 	for sentence in directions:
 		sentence = ''.join(ch for ch in sentence if ch not in exclude)
 		w = sentence.split()
 		for word in w:
 			word = word.lower()
-			if word not in words and word not in ignoreWords:
+			if word not in words: #and word not in ignoreWords:
 				words.append(word)
+
 	for word in words:
 		nextWordIndex = words.index(word)+1
+		double_word =''
+
 		try:
-			potentialIng = word+' '+words[nextWordIndex]
+			double_word = word+' '+words[nextWordIndex]
 		except:
-			potentialIng = word
-		if word in lists.tools:
-			if potentialIng not in ingredientsList:
-				parsed['tools'].append(word)
-			
-		if word in lists.methods:
-			if potentialIng not in ingredientsList:
+			double_word = word
+		try:
+			lastTool = parsed['tools'][-1]
+		except:
+			lastTool = ''
+
+		if word not in lastTool:
+			if double_word in lists.tools:
+				parsed['tools'].append(double_word)
+			elif word in lists.tools:
+				if double_word not in ingredientsList:
+					parsed['tools'].append(word)
+
+		if double_word in lists.methods:
+			parsed['methods'].append(double_word)
+
+		elif word in lists.methods:
+			if double_word not in ingredientsList:
 				parsed['methods'].append(word)
+
 	print words
 	print parsed['tools']
 	print parsed['methods']
@@ -184,75 +199,45 @@ def main(recipeURL):
 	print recipe.unicode
 
 def tokenizeLine(string):
-	
 	line = string.split('^')
 	numArgs = len(line)
 	out = []
-
 	for item in line:
-
 		if item == '':
-
 			out.append(item)
-
 		else:
-
 			if item[0] != '~':
-
 				out.append(item)
-
 			else:
-
 				text = item.split('~')
 				empty = False
 				appended = False
-
 				for word in text:
-
 					if len(text) == 3 and text[1] == word and word != '':
-
 						out.append(word)
 						appended = True
-
 					if empty and word == '' and not appended:
-
 						out.append(word)
 						empty = False
 						appended = True
-
 					else:
-
 						if word == '':
-
 							empty = True
-
 	return out
 
 def readIngredientFromLine(line):
-
 	#fields = 0
-
 	#if objectType == 'FOOD':
-		
 	#	isFood = True
 	#	fields = 14
-
 	#elif objectType == 'NUTIRENT':
-
 	#	isNutrient = True
 	#	fields = 6
-
 	#else:
-
 	#	print('objectType not recognized.')
 	#	return None
-
 	tokens = tokenizeLine(line)
-
-	
-
 	output = objects.ingredient()
-
 	#	output.id = tokens[0]
 	output.category = tokens[1]
 	output.descriptor = tokens[2].lower()
@@ -267,25 +252,16 @@ def readIngredientFromLine(line):
 	#	output.proFactor = tokens[11]
 	#	output.fatFactor = tokens[12]
 	#	output.carbFactor = tokens[13]
-
 	return output
 
 def readIngredientsFromFile(fileName):
-
 	ingredientList = []
-
 	with open(fileName, 'r') as f:
-
 		while(True):
-
 			line = f.readline()
-
 			if not line:
-
 				break
-
 			ingredientList.append(readIngredientFromLine(line))
-
 	return ingredientList
 
 #main(sys.argv[1])
