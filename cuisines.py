9d9fe2cdd07363b814b1b24ce67f235c6aa1c895
Salil Gupta <salil.gupta323@gmail.com>
2014-03-17 13:22:55 -0500

    Built out the cheese replacement

--- cuisines.py
+++ cuisines.py
@@ -1,5 +1,5 @@
 from lists import cuisines as cuisines
-import lists, scraper, parsing
+import lists, scraper, parsing,re
 # Lists
 from lists import poultryAndGame as poultryAndGame,\
 livestock as livestock, \
@@ -8,6 +8,8 @@ seafood as seafood
 
 
 meatsCategories = ['0500', '0700', '1000','1300','1700']
+cheeseCategory = ['0100']
+sauceCategory = ['0600']
 
 def cuisineChange(recipe, cuisineType):
 	if cuisineType == "indian":
@@ -20,15 +22,17 @@ def cuisineChange(recipe, cuisineType):
 def changeToIndian(recipe):
 	ingredients = recipe.ingredients
 	cuisineMeats = cuisines["indian"]["meats"]
+	cuisineCheese = cuisines['indian']['cheese']
 	for ing in ingredients:
 		# category
 		category = ing.category
+		#check the meats used in recipe
 		if category in meatsCategories:
 			print ing.unicode()
 			# name
 			meat = ing.name
-			meatInDescriptor = findMeatDescriptor(ing.descriptor, cuisineMeats)
-			print 'MEATS: ', meat, ", ", meatInDescriptor
+			meatInDescriptor = findInDescriptor(ing.descriptor, cuisineMeats)
+			#print 'MEATS: ', meat, ", ", meatInDescriptor
 			if meat not in cuisineMeats and meatInDescriptor not in cuisineMeats:
 				# Replace with meat in cuisineMeats
 				if meat in (poultryAndGame + seafood) or meatInDescriptor in (poultryAndGame + seafood):
@@ -37,23 +41,76 @@ def changeToIndian(recipe):
 					ing.descriptor = "boneless"
 				else:
 					# lamb/mutton
-					ing.name = "mutton"
-					ing.descriptor = ""	
+					ing.name = "mutton"	
 				print ing.unicode()
+		#replace cheese with indian cheese
+		if category in cheeseCategory:
+			pat = re.search("cheese*",ing.name.lower())
+			pat2 = re.search("cheese*",ing.descriptor.lower())
+			if pat or pat2:
+				print ing.unicode()
+				cheese = ing.name
+				cheeseInDescriptor = findInDescriptor(ing.descriptor,cuisineCheese) 
+				#print 'CHEESE: ', cheese, ", ", cheeseInDescriptor
+				if cheese not in cuisineCheese and cheeseInDescriptor not in cuisineCheese:
+					#replace cheese with indian cheese
+					ing.name = "cottage cheese"
+					ing.descriptor = ""
+					print ing.unicode()
+		#replace bread with naan if not pastry (i.e. cake, pie)... this is tough....
+		#replace sauces....
+
+'''
+def changeToMexican(recipe):
+	ingredients = recipe.ingredients
+	cuisineMeats = cuisines["mexican"]["meats"]
+	cuisineCheese = cuisines['mexican']['cheese']
+	for ing in ingredients:
+		# category
+		category = ing.category
+		#check the meats used in recipe
+		if category in meatsCategories:
+			print ing.unicode()
+			# name
+			meat = ing.name
+			meatInDescriptor = findInDescriptor(ing.descriptor, cuisineMeats)
+			#print 'MEATS: ', meat, ", ", meatInDescriptor
+			if meat not in cuisineMeats and meatInDescriptor not in cuisineMeats:
+				# Replace with meat in cuisineMeats
+				if meat in (poultryAndGame + seafood) or meatInDescriptor in (poultryAndGame + seafood):
+					# chicken
+					ing.name = "chicken"
+					ing.descriptor = "boneless"
+				else:
+					# lamb/mutton
+					ing.name = "pork"	
+				print ing.unicode()
+		#replace cheese with indian cheese
+		if category in cheeseCategory:
+			pat = re.search("cheese*",ing.name.lower())
+			pat2 = re.search("cheese*",ing.descriptor.lower())
+			if pat or pat2:
+				print ing.unicode()
+				cheese = ing.name
+				cheeseInDescriptor = findInDescriptor(ing.descriptor,cuisineCheese) 
+				#print 'CHEESE: ', cheese, ", ", cheeseInDescriptor
+				if cheese not in cuisineCheese and cheeseInDescriptor not in cuisineCheese:
+					#replace cheese with indian cheese
+					ing.name = "oaxaca cheese"
+					ing.descriptor = ""
+					print ing.unicode()
+'''
 
 
-	
-
-def findMeatDescriptor(descriptor, cuisineMeats):
+def findInDescriptor(descriptor, cuisineList):
 	words = descriptor.split(' ')
-	meat = ''
+	x = ''
 	# print words
 	for word in words:
-		if word in cuisineMeats:
-			meat = word
+		if word in cuisineList:
+			x = word
 			break
-	return meat
-
+	return x
 ##### RECIPE INFO ######
 def getRecipe(recipeURL):
 	#temporary
@@ -67,8 +124,8 @@ def getRecipe(recipeURL):
 
 def main():
 	# recipe = getRecipe('http://allrecipes.com/recipe/chicken-stir-fry-3/')
-	recipe = getRecipe('http://allrecipes.com/Recipe/Flavorful-Beef-Stir-Fry-3/Detail.aspx?event8=1&prop24=SR_Thumb&e11=beef%20stir%20fry&e8=Quick%20Search&event10=1&e7=Recipe&soid=sr_results_p1i2')
-	
+	#recipe = getRecipe('http://allrecipes.com/Recipe/Flavorful-Beef-Stir-Fry-3/Detail.aspx?event8=1&prop24=SR_Thumb&e11=beef%20stir%20fry&e8=Quick%20Search&event10=1&e7=Recipe&soid=sr_results_p1i2')
+	recipe = getRecipe('http://allrecipes.com/recipe/shepherds-pie-vi/')
 	cuisineChange(recipe, "indian")
 
 main()
