4027c21a33b7ce90811ceac32fa147287bb60248
Salil Gupta <salil.gupta323@gmail.com>
2014-03-18 00:38:08 -0500

    Prompts the user for basic inputs and runs the command from there

---
+++ main.py
@@ -0,0 +1,38 @@
+import lists,scraper,transformer,parsing,objects,cuisines
+cuisineTypes = ['indian','mexican','chinese']
+def main():
+	lists.ingredientDB = parsing.readIngredientsFromFile('FOOD_DATA/FOOD_DES.txt')
+	lists.updateNameDB()
+
+	#Welcome message
+	print "Welcome to SVJ Recipe Transformer created by Josiah Evans, Vanessa Fang and Salil Gupta"
+	#Recipe input
+	recipeURL = raw_input("Please input a recipe url from allrecipes.com: ")
+	recipeInfo = scraper.retrieveRecipe(recipeURL)
+	recipe = parsing.buildRecipeObject(recipeInfo)
+
+	print "Thanks! Now choose a transformation.\n We have three options:\n For a vegetarian tranformation type 0\n For a healthy transformation type 1\n For a cuisine transformation type 2"
+	transformationType = int(raw_input("Transformation Selection: "))
+	if transformationType ==0:
+		print "Thanks! You have selected a veggie transformation. We are processing your request!"
+		vegRecipe = transformer.veggieTransformer(recipe)
+		transformer.printRecipe(vegRecipe.name,vegRecipe.ingredients,vegRecipe.steps)
+	elif transformationType ==1:
+		print "Thanks! You have selected a healthy transformation. We are processing your request!"
+		healthyRecipe = transformer.healthyTransformer(recipe)
+	elif transformationType ==2:
+		print "Thanks! You have selected a cuisine transformation. Please choose a cuisine type:\n For Indian type 0 \n For Mexican type 1 \n for Chinese Type 2"
+		cuisineType = int(raw_input("Cuisine Type: "))
+
+		if cuisineType ==0:
+			cuisines.cuisineChange(recipe,cuisineTypes[0])
+		elif cuisineType ==1:
+			cuisines.cuisineChange(recipe,cuisineTypes[1])
+		elif cuisineType == 2:
+			cuisines.cuisineChange(recipe,cuisineTypes[2])
+		else:
+			print "Sorry! But you must have typed an incorrect cuisine number!"
+	else:
+		print "Sorry! But you must have typed an incorrect transformation number!"
+
+main()
