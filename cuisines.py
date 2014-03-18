303994ad39d1267031f1bb442be427e654ac3682
Salil Gupta <salil.gupta323@gmail.com>
2014-03-17 23:12:25 -0500

    Completed all cuisine transformations

--- cuisines.py
+++ cuisines.py
@@ -7,7 +7,6 @@ stocks as stocks, \
 seafood as seafood,\
 meats as meats
 
-
 meatsCategories = ['0500', '0700', '1000','1300','1700']
 cheeseCategory = ['0100']
 spiceHerbsCategory = ['0200']
@@ -23,213 +22,150 @@ def cuisineChange(recipe, cuisineType):
 		changeToMexican(recipe)
 
 def changeToIndian(recipe):
+	cuisine = "indian"
 	ingredients = recipe.ingredients
-	cuisineMeats = cuisines["indian"]["meats"]
-	cuisineCheese = cuisines['indian']['cheese']
-	cuisineSpiceHerb = cuisines['indian']['spicesAndHerbs']
-	cuisineSauces = cuisines['indian']['sauces']
-	cuisineVegGarn = cuisines['indian']['veggiesAndGarnish']
-	removeIngredients=[]
+	cuisineMeats = cuisines[cuisine]["meats"]
+	cuisineCheese = cuisines[cuisine]['cheese']
+	cuisineSpiceHerb = cuisines[cuisine]['spicesAndHerbs']
+	cuisineSauces = cuisines[cuisine]['sauces']
+	cuisineVegGarn = cuisines[cuisine]['veggiesAndGarnish']
+
+	modifiedIngredients=[]
 	addSpices = False
 	vegIndex = 0
 	for ing in ingredients:
-		# category
 		category = ing.category
 		#check the meats used in recipe
-		if category in meatsCategories:
-			#print ing.unicode()
-			# name
-			meat = ing.name
-			meatInDescriptor = findInDescriptor(ing.descriptor, cuisineMeats)
-			#print 'MEATS: ', meat, ", ", meatInDescriptor
-			if meat not in cuisineMeats and meatInDescriptor not in cuisineMeats:
-				# Replace with meat in cuisineMeats
+		if category in meatsCategories or ing.name in meats:
+
+			if fitsCuisineForCategory(ing,cuisineMeats):
+				(meat,meatInDescriptor) = findIngNames(ing,cuisineMeats)
 				if meat in (poultryAndGame + seafood) or meatInDescriptor in (poultryAndGame + seafood):
 					# chicken
-					removeIngredients.append({ing.name+"$"+ing.descriptor:"chicken"})
-					ing.name = "chicken"
-					ing.descriptor = "boneless"
+					modifiedIngredients.append(updateIngredient(ing,cuisineMeats[0],"boneless",""))
 				else:
 					# lamb/mutton
-					removeIngredients.append({ing.name+"$"+ing.descriptor:"mutton"})
-					ing.name = "mutton"	
-				#print ing.unicode()
+					modifiedIngredients.append(updateIngredient(ing, cuisineMeats[1], "", ""))
+
 		#replace cheese with indian cheese
-		if category in cheeseCategory:
+		elif category in cheeseCategory:
 			pat = re.search("cheese*",ing.name.lower())
 			pat2 = re.search("cheese*",ing.descriptor.lower())
 			if pat or pat2:
-				#print ing.unicode()
-				cheese = ing.name
-				cheeseInDescriptor = findInDescriptor(ing.descriptor,cuisineCheese) 
-				#print 'CHEESE: ', cheese, ", ", cheeseInDescriptor
-				if cheese not in cuisineCheese and cheeseInDescriptor not in cuisineCheese:
-					#replace cheese with indian cheese
-					removeIngredients.append({ing.name+"$"+ing.descriptor:"cottage cheese (paneer)"})
-					ing.name = "cottage cheese (paneer)"
-					ing.descriptor = ""
-					#print ing.unicode()
-		if category in spiceHerbsCategory:
-			#print ing.unicode()
-			spiceHerb = ing.name
-			spiceInDescriptor = findInDescriptor(ing.descriptor,cuisineSpiceHerb)
-			if spiceHerb not in cuisineSpiceHerb and spiceInDescriptor not in cuisineSpiceHerb:
-				if not addSpices:
-					addSpices = True
-					removeIngredients.append({ing.name+"$"+ing.descriptor: "turmeric, cumin, garam masala"})
-					turmeric = parsing.findIngredient('turmeric')
-					cumin = parsing.findIngredient('cumin')
-					garam_masala = objects.Ingredient(name='garam masala')
-					ing.name = ""
-					#print ing.name
-					#ingredients.remove(ing)
-					ingredients+=[turmeric,cumin,garam_masala]
-				else:
-					ing.name = ""
-					removeIngredients.append({ing.name+"$"+ing.descriptor: ""})
-		#replace bread with naan if not pastry (i.e. cake, pie)... this is tough....
-		#replace sauces....
-		if category in sauce or ing.name in stocks:
-			soupSauces = ing.name
-			sauceInDescriptor = findInDescriptor(ing.descriptor,cuisineSauces)
-			#print 'Sauces:',soupSauces, "Descriptor:", sauceInDescriptor
-			if soupSauces not in cuisineSauces and sauceInDescriptor not in cuisineSauces:
-				removeIngredients.append({ing.name+"$"+ing.descriptor:"curry sauce"})
-				ing.name= "curry sauce"
-				ing.descriptor = "tomato puree based"
+				if fitsCuisineForCategory(ing,cuisineCheese):
+					(cheese,cheeseInDescriptor) = findIngNames(ing,cuisineCheese)
+					modifiedIngredients.append(updateIngredient(ing,cuisineCheese[0],"",""))
 
-		if category in veggiesGarnish:
+		#replace spices and herbs
+		elif category in spiceHerbsCategory:
+			if fitsCuisineForCategory(ing,cuisineSpiceHerb):
+				if not addSpices:
+					addSpices= True
+					modifiedIngredients.append({ing.name+"$"+ing.descriptor:', '.join(cuisineSpiceHerb[0:3])})
+					modifiedIngredients.append(updateIngredient(ing,"","",""))
+				else:
+					modifiedIngredients.append(updateIngredient(ing, "", "", ""))
+		
+		#replace sauces....
+		elif category in sauce or ing.name in stocks:
+			if fitsCuisineForCategory(ing,cuisineSauces):
+				modifiedIngredients.append(updateIngredient(ing,cuisineSauces[0],"tomato puree based",""))
+
+		elif category in veggiesGarnish:
 			veggieGarName = ing.name
 			veggieDescriptor = ing.descriptor
-			#veggieDescriptor = findInDescriptor(ing.descriptor,cuisineVegGarn)
-			isVeggie = findVeggies(veggieGarName,veggieDescriptor,"indian")
-			#print "BEFORE:",ing.unicode()
+			isVeggie = findVeggies(veggieGarName,veggieDescriptor,cuisine)
 			if not isVeggie:
 				if vegIndex < len(cuisineVegGarn):
-					removeIngredients.append({ing.name+"$"+ing.descriptor:cuisineVegGarn[vegIndex]})
-					ing.name = cuisineVegGarn[vegIndex]
+					modifiedIngredients.append(updateIngredient(ing,cuisineVegGarn[vegIndex],"",""))
 					vegIndex+=1
-					#print "AFTER:",ing.unicode()
 				else:
-					ing.name = ""
-					removeIngredients.append({ing.name+"$"+ing.descriptor:""})
+					modifiedIngredients.append(updateIngredient(ing,"","",""))
+	if addSpices:
+		for spiceHerb in cuisineSpiceHerb[0:3]:
+			newIng=parsing.findIngredient(spiceHerb)
+			if not newIng.name:
+				newIng = objects.Ingredient(name=spiceHerb)
+			ingredients.append(newIng)
 
-			#if soupSauces not in cuisineSauces and sauceInDescriptor not in cuisineSauces:
-	#recipe.ingredients = removeIngredient(ingredients)
 	recipe.ingredients = sanitizeIngredients(ingredients)
-	recipe.steps = replaceDirections(removeIngredients,recipe.steps)
+	recipe.steps = replaceDirections(modifiedIngredients,recipe.steps)
 	recipe.name = "Indian Version of -"+recipe.name
 	print recipe.unicode()
 
-def removeIngredient(ingredients):
-	finalIng = []
-	for ing in ingredients:
-		if ing.name:
-			finalIng.append(ing)
-	return finalIng
-
-def replaceDirections(ingredients,steps):
-	for ing in ingredients:
-		for key,value in ing.iteritems():
-			for step in steps:
-				splitKey = key.split('$')
-				name = splitKey[0]
-				descriptor = splitKey[1]
-				print 'DIR: ', name, ' ', descriptor
-				if re.search("(?i)(%s|%s)" % (name, descriptor+name), step.direction):
-					step.direction = re.sub("(?i)(%s|%s)" % (name, descriptor+name), value, step.direction)
-	return steps
-
-'''
 def changeToMexican(recipe):
+	cuisine = "mexican"
 	ingredients = recipe.ingredients
-	cuisineMeats = cuisines["mexican"]["meats"]
-	cuisineCheese = cuisines['mexican']['cheese']
+	cuisineMeats = cuisines[cuisine]["meats"]
+	cuisineCheese = cuisines[cuisine]['cheese']
+	cuisineSpiceHerb = cuisines[cuisine]['spicesAndHerbs']
+	cuisineSauces = cuisines[cuisine]['sauces']
+	cuisineVegGarn = cuisines[cuisine]['veggiesAndGarnish']
+
+	modifiedIngredients=[]
+	addSpices = False
+	vegIndex = 0
 	for ing in ingredients:
-		# category
 		category = ing.category
 		#check the meats used in recipe
-		if category in meatsCategories:
-			print ing.unicode()
-			# name
-			meat = ing.name
-			meatInDescriptor = findInDescriptor(ing.descriptor, cuisineMeats)
-			#print 'MEATS: ', meat, ", ", meatInDescriptor
-			if meat not in cuisineMeats and meatInDescriptor not in cuisineMeats:
-				# Replace with meat in cuisineMeats
+		if category in meatsCategories or ing.name in meats:
+
+			if fitsCuisineForCategory(ing,cuisineMeats):
+				(meat,meatInDescriptor) = findIngNames(ing,cuisineMeats)
 				if meat in (poultryAndGame + seafood) or meatInDescriptor in (poultryAndGame + seafood):
 					# chicken
-					ing.name = "chicken"
-					ing.descriptor = "boneless"
+					modifiedIngredients.append(updateIngredient(ing,cuisineMeats[0],"boneless",""))
 				else:
 					# lamb/mutton
-					ing.name = "pork"	
-				print ing.unicode()
+					modifiedIngredients.append(updateIngredient(ing, cuisineMeats[1], "barbacoa", ""))
+
 		#replace cheese with indian cheese
-		if category in cheeseCategory:
+		elif category in cheeseCategory:
 			pat = re.search("cheese*",ing.name.lower())
 			pat2 = re.search("cheese*",ing.descriptor.lower())
 			if pat or pat2:
-				print ing.unicode()
-				cheese = ing.name
-				cheeseInDescriptor = findInDescriptor(ing.descriptor,cuisineCheese) 
-				#print 'CHEESE: ', cheese, ", ", cheeseInDescriptor
-				if cheese not in cuisineCheese and cheeseInDescriptor not in cuisineCheese:
-					#replace cheese with indian cheese
-					ing.name = "oaxaca cheese"
-					ing.descriptor = ""
-					print ing.unicode()
-'''
+				if fitsCuisineForCategory(ing,cuisineCheese):
+					(cheese,cheeseInDescriptor) = findIngNames(ing,cuisineCheese)
+					modifiedIngredients.append(updateIngredient(ing,cuisineCheese[0],"",""))
 
-def findVeggies(name,descriptor,cuisine):
-	for veggie in cuisines[cuisine]['veggiesAndGarnish']:
-		pat = re.search("%s.*" % veggie,name)
-		pat2 = re.search("%s.*" % veggie,descriptor)
-		if pat:
-			return True
-		elif pat2:
-			return True
+		#replace spices and herbs
+		elif category in spiceHerbsCategory:
+			if fitsCuisineForCategory(ing,cuisineSpiceHerb):
+				if not addSpices:
+					addSpices= True
+					modifiedIngredients.append({ing.name+"$"+ing.descriptor:', '.join(cuisineSpiceHerb)})
+					modifiedIngredients.append(updateIngredient(ing,"","",""))
+				else:
+					modifiedIngredients.append(updateIngredient(ing, "", "", ""))
+		
+		#replace sauces....
+		elif category in sauce:
+			if fitsCuisineForCategory(ing,cuisineSauces):
+				modifiedIngredients.append(updateIngredient(ing,cuisineSauces[0],"",""))
 
-def findInDescriptor(descriptor, cuisineList):
-	words = descriptor.split(' ')
-	x = ''
-	# print words
-	for word in words:
-		if word in cuisineList:
-			x = word
-			break
-	return x
+		elif category in veggiesGarnish:
+			veggieGarName = ing.name
+			veggieDescriptor = ing.descriptor
+			isVeggie = findVeggies(veggieGarName,veggieDescriptor,cuisine)
+			if not isVeggie:
+				if vegIndex < len(cuisineVegGarn):
+					modifiedIngredients.append(updateIngredient(ing,cuisineVegGarn[vegIndex],"",""))
+					vegIndex+=1
+				else:
+					modifiedIngredients.append(updateIngredient(ing,"","",""))
+	if addSpices:
+		for spiceHerb in cuisineSpiceHerb[0:3]:
+			newIng=parsing.findIngredient(spiceHerb)
+			if not newIng.name:
+				newIng = objects.Ingredient(name=spiceHerb)
+			ingredients.append(newIng)
 
-def sanitizeIngredients(ingredientsList):
-	uniqueIng = []
-	finalIng = []
-	for ing in ingredientsList:
-		if not ing.name in uniqueIng and ing.name:
-			uniqueIng.append(ing.name)
-			finalIng.append(ing)
-	return finalIng
+	recipe.ingredients = sanitizeIngredients(ingredients)
+	recipe.steps = replaceDirections(modifiedIngredients,recipe.steps)
+	recipe.name = "Mexican Version of - "+recipe.name
+	print recipe.unicode()
 
 ##### CHINESE ######
-def updateIngredient(ingredient, name, descriptor, preparation):
-	origName = ingredient.name
-	origDesc = ingredient.descriptor
-	ingredient.name = name
-	ingredient.descriptor = descriptor
-	ingredient.preparation = preparation
-
-	return {origName + "$" + origDesc:name}
-
-def findIngNames(ing, category):
-	return (ing.name, findInDescriptor(ing.descriptor, category))
-
-def fitsCuisineForCategory(ing, category):
-	(name, nameInDescriptor) = findIngNames(ing, category)
-	fits = False
-	if name not in category and nameInDescriptor not in category:
-		fits = True
-	
-	return fits
-
 def changeToChinese(recipe):
 	cuisine = "chinese"
 	ingredients = recipe.ingredients
@@ -263,7 +199,7 @@ def changeToChinese(recipe):
 				else:
 					modifiedIngredients.append(updateIngredient(ing, "", "", ""))
 
-		elif category in sauce or ing.name in stocks:
+		elif category in sauce:
 			if fitsCuisineForCategory(ing, cuisineSauces):
 				modifiedIngredients.append(updateIngredient(ing, cuisineSauces[0], "", ""))
 
@@ -285,6 +221,8 @@ def changeToChinese(recipe):
 	if addSpices:
 		for spiceHerb in cuisineSpiceHerb:
 			newIng = parsing.findIngredient(spiceHerb)
+			if not newIng.name:
+				newIng = objects.Ingredient(name = spiceHerb)
 			ingredients.append(newIng)
 
 	recipe.ingredients = sanitizeIngredients(ingredients)
@@ -293,10 +231,74 @@ def changeToChinese(recipe):
 	print recipe.unicode()
 	return recipe
 
+##HELPER FUNCTIONS##
+#goes through the directions and replaces old ingredients with new ingredients
+def replaceDirections(ingredients,steps):
+	for ing in ingredients:
+		for key,value in ing.iteritems():
+			for step in steps:
+				splitKey = key.split('$')
+				name = splitKey[0]
+				descriptor = splitKey[1]
+				#print 'DIR: ', name, ' ', descriptor
+				if re.search("(?i)(%s|%s)" % (name, descriptor+name), step.direction):
+					step.direction = re.sub("(?i)(%s|%s)" % (name, descriptor+name), value, step.direction)
+	return steps
+#searches for veggies plural in name and descriptor
+def findVeggies(name,descriptor,cuisine):
+	for veggie in cuisines[cuisine]['veggiesAndGarnish']:
+		pat = re.search("%s.*" % veggie,name)
+		pat2 = re.search("%s.*" % veggie,descriptor)
+		if pat:
+			return True
+		elif pat2:
+			return True
+
+#searches for name of ingredient in descriptor
+def findInDescriptor(descriptor, cuisineList):
+	words = descriptor.split(' ')
+	x = ''
+	# print words
+	for word in words:
+		if word in cuisineList:
+			x = word
+			break
+	return x
+
+#removes duplicate ingredients and any ingredients that need to be tossed out
+def sanitizeIngredients(ingredientsList):
+	uniqueIng = []
+	finalIng = []
+	for ing in ingredientsList:
+		if not ing.name in uniqueIng and ing.name:
+			uniqueIng.append(ing.name)
+			finalIng.append(ing)
+	return finalIng
+
+#stores a modification of an old ingredient with the new ingredient
+def updateIngredient(ingredient, name, descriptor, preparation):
+	origName = ingredient.name
+	origDesc = ingredient.descriptor
+	ingredient.name = name
+	ingredient.descriptor = descriptor
+	ingredient.preparation = preparation
+
+	return {origName + "$" + origDesc:name}
+
+#gives name and name of ingredient from findInDescriptor
+def findIngNames(ing, category):
+	return (ing.name, findInDescriptor(ing.descriptor, category))
+
+#decides if the ingredient found fits the cuisine or not
+def fitsCuisineForCategory(ing, category):
+	(name, nameInDescriptor) = findIngNames(ing, category)
+	fits = False
+	if name not in category and nameInDescriptor not in category:
+		fits = True
+	return fits
 
 ##### RECIPE INFO ######
 def getRecipe(recipeURL):
-	#temporary
 	lists.ingredientDB = parsing.readIngredientsFromFile('FOOD_DATA/FOOD_DES.txt')
 	lists.updateNameDB()
 	###
@@ -306,29 +308,10 @@ def getRecipe(recipeURL):
 	return recipe
 
 def main():
-	# recipe = getRecipe('http://allrecipes.com/recipe/chicken-stir-fry-3/')
+	recipe = getRecipe('http://allrecipes.com/recipe/chicken-stir-fry-3/')
 	#recipe = getRecipe('http://allrecipes.com/Recipe/Flavorful-Beef-Stir-Fry-3/Detail.aspx?event8=1&prop24=SR_Thumb&e11=beef%20stir%20fry&e8=Quick%20Search&event10=1&e7=Recipe&soid=sr_results_p1i2')
-	recipe = getRecipe('http://allrecipes.com/Recipe/Italian-Sausage-Soup-with-Tortellini/Detail.aspx?event8=1&prop24=SR_Title&e11=soups&e8=Quick%20Search&event10=1&e7=Recipe&soid=sr_results_p1i3')
+	#recipe = getRecipe('http://allrecipes.com/Recipe/Italian-Sausage-Soup-with-Tortellini/Detail.aspx?event8=1&prop24=SR_Title&e11=soups&e8=Quick%20Search&event10=1&e7=Recipe&soid=sr_results_p1i3')
 	#print recipe.unicode()
-	cuisineChange(recipe, "chinese")
+	cuisineChange(recipe, "mexican")
 
 main()
-
-# Go through ings
-	# Check category, name, descriptor
-	# If meat not in cuisine meats
-		# Replace meat
-			# Poultry + fish -> chicken
-			# Else -> non chicken
-	# Cheese
-		# Indian, all cheese -> cottage cheese
-		# Asian, remove all cheese
-		# Mexican, oaxaca cheese
-	# Breads
-		# Indian, naan
-		# Chinese, ignore
-		# Mexican, tortilla
-	# Veggies
-		# If not veggie list
-			# Replace
-
