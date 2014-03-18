3e85a222be3388cf231733c460087d1b1970bc26
Salil Gupta <salil.gupta323@gmail.com>
2014-03-18 14:46:50 -0500

    added a debug functionality:

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ cuisineTypes = ['indian','mexican','chinese']
 def main():
 	lists.ingredientDB = parsing.readIngredientsFromFile('FOOD_DATA/FOOD_DES.txt')
 	lists.updateNameDB()
+	debug = True
 
 	#Welcome message
 	print "Welcome to SVJ Recipe Transformer created by Josiah Evans, Vanessa Fang and Salil Gupta"
 	#Recipe input
-	recipeURL = raw_input("Please input a recipe url from allrecipes.com: ")
-	recipeInfo = scraper.retrieveRecipe(recipeURL)
-	recipe = parsing.buildRecipeObject(recipeInfo)
+	 if not debug:
+        recipeURL = raw_input("Please input a recipe url from allrecipes.com: ")
+    else:
+        recipeURL = 'http://allrecipes.com/recipe/white-chocolate-raspberry-cheesecake/detail.aspx?src=VD_Summary'
+
+    print 'Loading recipe from allrecipes.com...'
+    recipeInfo = scraper.retrieveRecipe(recipeURL)
+    print 'Building recipe object...'
+    recipe = parsing.buildRecipeObject(recipeInfo)
 
 	print "Thanks! Now choose a transformation.\n We have three options:\n For a vegetarian tranformation (or 'un-vegetarian') type 0\n For a healthy transformation type 1\n For a cuisine transformation type 2"
 	transformationType = int(raw_input("Transformation Selection: "))
@@ -23,7 +30,7 @@ def main():
 	elif transformationType ==1:
 		print "Thanks! You have selected a healthy transformation. We are processing your request!"
 		healthyRecipe = healthyTransformer(recipe)
-		healthyTransformer.printRecipe(vegRecipe, "Healthy")
+		healthyTransformer.printRecipe(healthyRecipe, "Healthy")
 	elif transformationType ==2:
 		print "Thanks! You have selected a cuisine transformation. Please choose a cuisine type:\n For Indian type 0 \n For Mexican type 1 \n for Chinese Type 2"
 		cuisineType = int(raw_input("Cuisine Type: "))
